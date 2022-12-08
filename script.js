e068c77bb07cd8896dc1261726370bf7f637f261
Nik <n.koeck@gmx.de>
2022-12-08 12:13:07 +0100

    add rainbow option, add mousedown

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //Variable
 
 let pixel = 50;
+let defaultColor = "black";
+let selection = defaultColor;
 const main = document.getElementById("main");
 const grid = document.getElementById("grid");
 const reset = document.querySelector("#reset");
 const input = document.getElementById("pixelValue");
 const valueOut = document.getElementById("valueOut");
+const rgb = document.getElementById("rgb");
+const black = document.getElementById("black");
 
+//Mouse down
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 
 
@@ -21,32 +29,45 @@ function createDivArray(size){
         gridElement.style.width = widthHeight + "px";
         gridElement.style.height = widthHeight + "px";
         gridElement.addEventListener('mouseover', changingColor);
+        gridElement.addEventListener('mousedown', changingColor);
         grid.appendChild(gridElement);
     }
     changingColor();
 }
 
+
+
 function changingColor(e){
-    e.target.style.backgroundColor = 'green';
+    if (e.type === 'mouseover' && !mouseDown) return
+    if(selection === defaultColor) e.target.style.backgroundColor = defaultColor;
+    if(selection === "rgb"){
+        const randomR = Math.floor(Math.random() * 256);
+        const randomG = Math.floor(Math.random() * 256);
+        const randomB = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+    }
 }
 
 //EventListener
 input.addEventListener("change", () =>{
     pixel = input.value;
-    while(grid.firstChild != null){
-        grid.removeChild(grid.lastChild);
-    }
+    grid.innerHTML = '';
     createDivArray(pixel);
     valueOut.textContent = pixel;
-
 });
 
+rgb.onclick = () =>{
+    selection = "rgb"
+}
 
+black.onclick = () =>{
+    selection = defaultColor;
+}
 
-reset.addEventListener("click", () => { //Resetting the matrix to defaultColor
-    let children = grid.childNodes;
+reset.onclick = () => {
     grid.innerHTML = '';
-});
+    createDivArray(pixel);
+}
 
 window.onload = () =>{
     createDivArray(20);
