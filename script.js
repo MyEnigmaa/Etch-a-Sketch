524732bada18e04540a57ae15529979e436730ee
Nik <n.koeck@gmx.de>
2022-12-08 11:23:35 +0100

    add style, rework and clean code

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
+//Variable
+
+let pixel = 50;
+const main = document.getElementById("main");
+const grid = document.getElementById("grid");
+const reset = document.querySelector("#reset");
+const input = document.getElementById("pixelValue");
+const valueOut = document.getElementById("valueOut");
+
+
+
 
 //Functions
 
-function createDivArray(n){
-    let div = new Array(n*n);
-    for(i=0; i<n*n; i++){
-        let widthHeight = 600/n;
-        div[i] = document.createElement("div");
-        div[i].className = "child";
-        div[i].style.width = widthHeight + "px";
-        div[i].style.height = widthHeight + "px";
-        div[i].style.backgroundColor = defaultColor;
-        arrayDiv.appendChild(div[i]);
+function createDivArray(size){
+    
+    for(i=0; i<size*size; i++){
+        let widthHeight = 600/size;
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('child');
+        gridElement.style.width = widthHeight + "px";
+        gridElement.style.height = widthHeight + "px";
+        gridElement.addEventListener('mouseover', changingColor);
+        grid.appendChild(gridElement);
     }
+    changingColor();
 }
 
-
-//Variable
-
-let defaultColor = "aquamarine";
-let pixel = 50;
-const body = document.querySelector('body')
-const arrayDiv = document.createElement("div");
-const reset = document.querySelector("#reset");
-const input = document.getElementById("pixelValue");
-createDivArray(20);
-
-
-
-arrayDiv.id = "container";
-body.appendChild(arrayDiv);
-
+function changingColor(e){
+    e.target.style.backgroundColor = 'green';
+}
 
 //EventListener
 input.addEventListener("change", () =>{
     pixel = input.value;
-    while(arrayDiv.firstChild != null){
-        arrayDiv.removeChild(arrayDiv.lastChild);
+    while(grid.firstChild != null){
+        grid.removeChild(grid.lastChild);
     }
     createDivArray(pixel);
+    valueOut.textContent = pixel;
+
 });
 
-let children = arrayDiv.childNodes;
-children.forEach(child => {  //Coloring the matrix
-    child.addEventListener("mousemove", () => {
-        child.style["background-color"] = "green";
-        
-})});
+
 
 reset.addEventListener("click", () => { //Resetting the matrix to defaultColor
-    children.forEach(child => {
-        child.style.backgroundColor = defaultColor;
-    })
+    let children = grid.childNodes;
+    grid.innerHTML = '';
 });
+
+window.onload = () =>{
+    createDivArray(20);
+}
